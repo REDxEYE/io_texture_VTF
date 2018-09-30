797d4dcf418bc896e09cf07af0d449fb372305c1
RED_EYE <med45c@gmail.com>
2018-09-30 23:51:13 +0300

    int8 overflow fix

--- VTF.py
+++ VTF.py
@@ -72,6 +72,7 @@ class VTF:
 def export_texture(blender_texture,path):
     image_data = np.array(blender_texture.pixels,np.float)
     image_data = np.asarray(image_data*256,np.uint8)
+    image_data = image_data.clip(0,255)
     alpha_view = image_data[3::4]
     alpha_view[:] = 255
     def_options = vtf_lib.image_create_default_create_structure()
