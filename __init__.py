3eb2ceddfdeb990d0918e134983c6c45aed1bb1e
RED_EYE <med45c@gmail.com>
2018-09-30 22:38:57 +0300

    version fix

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 bl_info = {
     "name": "Source Engine VTF Texture import",
     "author": "RED_EYE",
-    "version": (0, 7),
+    "version": (0, 9),
     "blender": (2, 78, 0),
     'warning': 'May crash blender',
     "location": "File > Import-Export > Source Engine texture import (VTF)",
