ad621e29c3e558a691dc0e4e08cb0aba1c6c0ed4
RED_EYE <med45c@gmail.com>
2018-12-18 21:56:35 +0300

    Blender 2.8

--- __init__.py
+++ __init__.py
@@ -3,39 +3,43 @@ from pathlib import Path
 bl_info = {
     "name": "Source Engine VTF Texture import",
     "author": "RED_EYE",
-    "version": (1, 0,),
-    "blender": (2, 78, 0),
+    "version": (2, 0,),
+    "blender": (2, 80, 0),
     'warning': 'Uses a lot of ram (1Gb for 4k texture)',
     "location": "File > Import-Export > Source Engine texture import (VTF)",
     "description": "Import-Export Source Engine texture import (VTF)",
-    #"wiki_url": "http://www.barneyparker.com/blender-json-import-export-plugin",
-    #"tracker_url": "http://www.barneyparker.com/blender-json-import-export-plugin",
-    "category": "Import-Export"}
-from . import VTF
-from . import VMT
-from . import BlenderMaterial
+    # "wiki_url": "http://www.barneyparker.com/blender-json-import-export-plugin",
+    # "tracker_url": "http://www.barneyparker.com/blender-json-import-export-plugin",
+    "category": "Import-Export"
+}
+try:
+    from . import VTF
+    from . import VMT
+    from . import BlenderMaterial
+except:
+    import VTF,VMT,BlenderMaterial
 
 import bpy
 
-from bpy.props import StringProperty, BoolProperty,CollectionProperty, EnumProperty
-from bpy_extras.io_utils import ExportHelper
+from bpy.props import StringProperty, BoolProperty, CollectionProperty, EnumProperty
 
 import os.path
 import sys
+
 print('Appending "{}" to PATH'.format(os.path.abspath(os.path.dirname(__file__))))
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
-class VTFImporter(bpy.types.Operator):
+
+class VTFImporter_OT_operator(bpy.types.Operator):
     """Load Source Engine VTF texture"""
     bl_idname = "import_texture.vtf"
     bl_label = "Import VTF"
     bl_options = {'UNDO'}
 
     filepath = StringProperty(
-            subtype='FILE_PATH',
-            )
+        subtype='FILE_PATH',
+    )
     files = CollectionProperty(name='File paths', type=bpy.types.OperatorFileListElement)
-    
 
     load_alpha = BoolProperty(default=True, name='Load alpha into separate image')
     only_alpha = BoolProperty(default=False, name='Only load alpha')
@@ -44,7 +48,7 @@ class VTFImporter(bpy.types.Operator):
     def execute(self, context):
         directory = Path(self.filepath).parent.absolute()
         for file in self.files:
-            VTF.import_texture(str(directory / file.name),self.load_alpha,self.only_alpha)
+            VTF.import_texture(str(directory / file.name), self.load_alpha, self.only_alpha)
         return {'FINISHED'}
 
     def invoke(self, context, event):
@@ -52,33 +56,36 @@ class VTFImporter(bpy.types.Operator):
         wm.fileselect_add(self)
         return {'RUNNING_MODAL'}
 
-class VMTImporter(bpy.types.Operator):
+
+class VMTImporter_OT_operator(bpy.types.Operator):
     """Load Source Engine VMT material"""
     bl_idname = "import_texture.vmt"
     bl_label = "Import VMT"
     bl_options = {'UNDO'}
 
     filepath = StringProperty(
-            subtype='FILE_PATH',
-            )
+        subtype='FILE_PATH',
+    )
 
     filter_glob = StringProperty(default="*.vmt", options={'HIDDEN'})
-    game = StringProperty(name="PATH TO GAME",subtype='FILE_PATH',default = "" )
-    override = BoolProperty(default = False,name='Override existing?')
+    game = StringProperty(name="PATH TO GAME", subtype='FILE_PATH', default="")
+    override = BoolProperty(default=False, name='Override existing?')
 
     def execute(self, context):
-        vmt = VMT.VMT(self.filepath,self.game)
+        vmt = VMT.VMT(self.filepath, self.game)
         mat = BlenderMaterial.BlenderMaterial(vmt)
         mat.load_textures()
         if mat.create_material(self.override) == 'EXISTS' and not self.override:
-            self.report({'INFO'},'{} material already exists')
+            self.report({'INFO'}, '{} material already exists')
         return {'FINISHED'}
 
     def invoke(self, context, event):
         wm = context.window_manager
         wm.fileselect_add(self)
         return {'RUNNING_MODAL'}
-class VTFExport(bpy.types.Operator):
+
+
+class VTFExport_OT_operator(bpy.types.Operator):
     """Export VTF texture"""
     bl_idname = "export_texture.vtf"
     bl_label = "Export VTF"
@@ -92,32 +99,33 @@ class VTFExport(bpy.types.Operator):
     )
 
     filename = StringProperty(
-            name="File Name",
-            description="Name used by the exported file",
-            maxlen=255,
-            subtype='FILE_NAME',
-            )
+        name="File Name",
+        description="Name used by the exported file",
+        maxlen=255,
+        subtype='FILE_NAME',
+    )
 
     imgFormat = EnumProperty(
         name="VTF Type Preset",
         description="Choose a preset. It will affect the result's format and flags.",
         items=(('RGBA8888Simple', "RGBA8888 Simple", "RGBA8888 format, format-specific Eight Bit Alpha flag only"),
-               ('RGBA8888Normal', "RGBA8888 Normal Map", "RGBA8888 format, format-specific Eight Bit Alpha and Normal Map flags"),
+               ('RGBA8888Normal', "RGBA8888 Normal Map",
+                "RGBA8888 format, format-specific Eight Bit Alpha and Normal Map flags"),
                ('DXT1Simple', "DXT1 Simple", "DXT1 format, no flags"),
                ('DXT5Simple', "DXT5 Simple", "DXT5 format, format-specific Eight Bit Alpha flag only"),
                ('DXT1Normal', "DXT1 Normal Map", "DXT1 format, Normal Map flag only"),
                ('DXT5Normal', "DXT5 Normal Map", "DXT5 format, format-specific Eight Bit Alpha and Normal Map flags")),
         default='RGBA8888Simple',
-        )
+    )
 
     def execute(self, context):
         sima = context.space_data
         ima = sima.image
         if ima is None:
-            self.report({"ERROR_INVALID_INPUT"},"No Image provided")
+            self.report({"ERROR_INVALID_INPUT"}, "No Image provided")
         else:
             print(context)
-            VTF.export_texture(ima,self.filepath,self.imgFormat)
+            VTF.export_texture(ima, self.filepath, self.imgFormat)
         return {'FINISHED'}
 
     def invoke(self, context, event):
@@ -131,33 +139,40 @@ class VTFExport(bpy.types.Operator):
         else:
             self.filepath = os.path.join(os.path.dirname(self.filepath), self.filename + self.filename_ext)
 
-
         context.window_manager.fileselect_add(self)
         return {'RUNNING_MODAL'}
 
 
 def menu_import(self, context):
-    self.layout.operator(VTFImporter.bl_idname, text="VTF texture (.vtf)")
-    self.layout.operator(VMTImporter.bl_idname, text="VMT texture (.vmt)")
+    self.layout.operator(VTFImporter_OT_operator.bl_idname, text="VTF texture (.vtf)")
+    self.layout.operator(VMTImporter_OT_operator.bl_idname, text="VMT texture (.vmt)")
 
-def export(self,context):
+
+def export(self, context):
     curImg = context.space_data.image
     if curImg is None:
-        self.layout.operator(VTFExport.bl_idname,text='Export to VTF')
+        self.layout.operator(VTFExport_OT_operator.bl_idname, text='Export to VTF')
     else:
-        self.layout.operator(VTFExport.bl_idname,text='Export to VTF').filename = os.path.splitext(curImg.name)[0]
+        self.layout.operator(VTFExport_OT_operator.bl_idname, text='Export to VTF').filename = \
+        os.path.splitext(curImg.name)[0]
+
+
+classes = (VTFImporter_OT_operator, VMTImporter_OT_operator, VTFExport_OT_operator)
+register_, unregister_ = bpy.utils.register_classes_factory(classes)
 
 
 def register():
-    bpy.utils.register_module(__name__)
-    bpy.types.INFO_MT_file_import.append(menu_import)
+    register_()
+    # bpy.utils.register_module(__name__)
+    bpy.types.TOPBAR_MT_file_import.append(menu_import)
     bpy.types.IMAGE_MT_image.append(export)
 
 
 def unregister():
-    bpy.utils.unregister_module(__name__)
-    bpy.types.INFO_MT_file_import.remove(menu_import)
+    # bpy.utils.unregister_module(__name__)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_import)
     bpy.types.IMAGE_MT_image.remove(export)
+    unregister_()
 
 
 if __name__ == "__main__":
